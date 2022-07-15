3d9f7f71aa8526a5a88e640b67ebb9828026dc3b
Angelo Alcantara <angeloalcantara4@gmail.com>
2022-07-15 20:44:31 +1200

    More tests for ActionButtonGroup

--- vollball-stat-tracker-app/src/components/__tests__/action-button-group.test.tsx
+++ vollball-stat-tracker-app/src/components/__tests__/action-button-group.test.tsx
@@ -2,15 +2,41 @@ import { render, screen } from '@testing-library/react';
 import ActionButtonGroup from '../action-button-group';
 
 describe('<ActionButtonGroup/>', () => {
-  beforeEach(() => {
-    render(<ActionButtonGroup title="Test Title" />);
-  });
-
-  test('should have a title', () => {
-    const title = screen.getByRole('heading', {
-      name: /test title/i,
+  describe('with points enabled', () => {
+    beforeEach(() => {
+      render(<ActionButtonGroup title="Test Title" pointEnabled />);
     });
 
-    expect(title.textContent).toBe('Test Title');
+    test('should have a title', () => {
+      const title = screen.getByRole('heading', {
+        name: /test title/i,
+      });
+      expect(title.textContent).toBe('Test Title');
+    });
+
+    test('should have a point button', () => {
+      const pointButton = screen.getByRole('button', {
+        name: /point/i,
+      });
+      expect(pointButton).toBeVisible();
+    });
+  });
+
+  describe('without points enabled', () => {
+    beforeEach(() => {
+      render(<ActionButtonGroup title="Test Title" />);
+    });
+
+    test('should have a title', () => {
+      const title = screen.getByRole('heading', {
+        name: /test title/i,
+      });
+      expect(title.textContent).toBe('Test Title');
+    });
+
+    test("shouldn't have a point button", () => {
+      const pointButton = screen.queryByText('Point');
+      expect(pointButton).toBeNull();
+    });
   });
 });
