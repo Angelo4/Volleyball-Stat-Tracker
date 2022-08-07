56c6751906440854e6a991e6f76d11cf91b16769
Angelo Alcantara <angeloalcantara4@gmail.com>
2022-08-07 13:08:02 +1200

    Initial test usage of redux reducers

--- vollball-stat-tracker-app/src/app/components/action-button-group.tsx
+++ vollball-stat-tracker-app/src/app/components/action-button-group.tsx
@@ -1,72 +1,40 @@
 import { Button, ButtonGroup, Grid, Typography } from '@mui/material';
 import { useReducer } from 'react';
+import { useDispatch } from 'react-redux';
 import {
   VbActionResult,
   VbActionResultType,
 } from '../../models/VBActionResult';
+import {
+  incrementAttackError,
+  incrementAttackPoint,
+  incrementAttackSuccess,
+} from '../features/stats-keeper';
 
 type ActionButtonGroupProps = {
   title: string;
   pointEnabled?: boolean;
 };
 
-const initialState: VbActionResult = {
-  point: 0,
-  success: 0,
-  error: 0,
-};
-
-function reducer(state: typeof initialState, type: VbActionResultType) {
-  switch (type) {
-    case VbActionResultType.Point:
-      return {
-        ...state,
-        point: !!state.point ? state.point + 1 : 1,
-      };
-    case VbActionResultType.Success:
-      return {
-        ...state,
-        success: state.success + 1,
-      };
-    case VbActionResultType.Error:
-      return {
-        ...state,
-        error: state.error + 1,
-      };
-    default:
-      throw new Error();
-  }
-}
-
 const ActionButtonGroup = (props: ActionButtonGroupProps) => {
-  const [state, dispatch] = useReducer(reducer, initialState);
-
-  const resultPoint = () => {
-    dispatch(VbActionResultType.Point);
-    console.log(state);
-  };
-
-  const resultSuccess = () => {
-    dispatch(VbActionResultType.Success);
-    console.log(state);
-  };
-
-  const resultError = () => {
-    dispatch(VbActionResultType.Error);
-    console.log(state);
-  };
+  const dispatch = useDispatch();
 
   return (
     <Grid item xs={4}>
       <Typography variant="h5">{props.title}</Typography>
       <ButtonGroup>
         {!!props.pointEnabled && (
-          <Button color="success" onClick={resultPoint}>
+          <Button
+            color="success"
+            onClick={() => dispatch(incrementAttackPoint())}
+          >
             Point
           </Button>
         )}
-        <Button onClick={resultSuccess}>Success</Button>
-        <Button color="error" onClick={resultError}>
+        <Button onClick={() => dispatch(incrementAttackSuccess())}>
+          Success
+        </Button>
+        <Button color="error" onClick={() => dispatch(incrementAttackError())}>
           Error
         </Button>
       </ButtonGroup>
