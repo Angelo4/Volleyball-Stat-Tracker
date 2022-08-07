73f24b18a0d5f28f650b598b9bf13b8bc396b199
Angelo Alcantara <angeloalcantara4@gmail.com>
2022-08-07 16:52:39 +1200

    Use ActionCentreContainer on TrackGame feature

--- vollball-stat-tracker-app/src/features/TrackGame.tsx
+++ vollball-stat-tracker-app/src/features/TrackGame.tsx
@@ -1,85 +1,10 @@
-import { Container, Grid } from '@mui/material';
-import { useDispatch } from 'react-redux';
-import { statKeeperActions } from '../redux/StatsKeeper';
-import ActionButtonGroup from '../components/vb_action_center/ActionButtonGroup';
+import { Container } from '@mui/material';
+import ActionCentreContainer from '../components/vb_action_center/ActionCentreContainer';
 
 const TrackGame = () => {
-  const dispatch = useDispatch();
-
   return (
     <Container>
-      <Grid container spacing={2}>
-        <ActionButtonGroup
-          title="Attack"
-          pointEnabled
-          handlePointClick={() =>
-            dispatch(statKeeperActions.incrementAttackPoint())
-          }
-          handleSuccessClick={() => {
-            dispatch(statKeeperActions.incrementAttackSuccess());
-          }}
-          handleErrorClick={() => {
-            dispatch(statKeeperActions.incrementAttackError());
-          }}
-        />
-        <ActionButtonGroup
-          title="Block"
-          pointEnabled
-          handlePointClick={() =>
-            dispatch(statKeeperActions.incrementBlockPoint())
-          }
-          handleSuccessClick={() => {
-            dispatch(statKeeperActions.incrementBlockSuccess());
-          }}
-          handleErrorClick={() => {
-            dispatch(statKeeperActions.incrementBlockError());
-          }}
-        />
-        <ActionButtonGroup
-          title="Serve"
-          pointEnabled
-          handlePointClick={() =>
-            dispatch(statKeeperActions.incrementServePoint())
-          }
-          handleSuccessClick={() => {
-            dispatch(statKeeperActions.incrementServeSuccess());
-          }}
-          handleErrorClick={() => {
-            dispatch(statKeeperActions.incrementServeError());
-          }}
-        />
-        <ActionButtonGroup
-          title="Set"
-          pointEnabled
-          handlePointClick={() =>
-            dispatch(statKeeperActions.incrementSetPoint())
-          }
-          handleSuccessClick={() => {
-            dispatch(statKeeperActions.incrementSetSuccess());
-          }}
-          handleErrorClick={() => {
-            dispatch(statKeeperActions.incrementSetError());
-          }}
-        />
-        <ActionButtonGroup
-          title="Reception"
-          handleSuccessClick={() => {
-            dispatch(statKeeperActions.incrementReceptionSuccess());
-          }}
-          handleErrorClick={() => {
-            dispatch(statKeeperActions.incrementReceptionError());
-          }}
-        />
-        <ActionButtonGroup
-          title="Dig"
-          handleSuccessClick={() => {
-            dispatch(statKeeperActions.incrementDigSuccess());
-          }}
-          handleErrorClick={() => {
-            dispatch(statKeeperActions.incrementDigError());
-          }}
-        />
-      </Grid>
+      <ActionCentreContainer />
     </Container>
   );
 };
