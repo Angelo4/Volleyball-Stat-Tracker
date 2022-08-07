91215be062ebba6848171416cab0ca07c5b50e64
Angelo Alcantara <angeloalcantara4@gmail.com>
2022-08-07 16:06:52 +1200

    Dispatch actions on ActionButtonGroup components

--- vollball-stat-tracker-app/src/app/components/action-button-group.tsx
+++ vollball-stat-tracker-app/src/app/components/action-button-group.tsx
@@ -1,43 +1,27 @@
 import { Button, ButtonGroup, Grid, Typography } from '@mui/material';
-import { useReducer } from 'react';
-import { useDispatch } from 'react-redux';
-import {
-  VbActionResult,
-  VbActionResultType,
-} from '../../models/VBActionResult';
-import { statKeeperActions } from '../../redux/stats-keeper';
 
 type ActionButtonGroupProps = {
   title: string;
   pointEnabled?: boolean;
+  handlePointClick?: () => { payload: undefined; type: string };
+  handleSuccessClick: () => void;
+  handleErrorClick: () => void;
 };
 
 const ActionButtonGroup = (props: ActionButtonGroupProps) => {
-  const dispatch = useDispatch();
-
   return (
     <Grid item xs={4}>
       <Typography variant="h5">{props.title}</Typography>
       <ButtonGroup>
+        <Button color="error" onClick={props.handleErrorClick}>
+          Error
+        </Button>
+        <Button onClick={props.handleSuccessClick}>Success</Button>
         {!!props.pointEnabled && (
-          <Button
-            color="success"
-            onClick={() => dispatch(statKeeperActions.incrementAttackPoint())}
-          >
+          <Button color="success" onClick={props.handlePointClick}>
             Point
           </Button>
         )}
-        <Button
-          onClick={() => dispatch(statKeeperActions.incrementAttackSuccess())}
-        >
-          Success
-        </Button>
-        <Button
-          color="error"
-          onClick={() => dispatch(statKeeperActions.incrementAttackError())}
-        >
-          Error
-        </Button>
       </ButtonGroup>
     </Grid>
   );
--- vollball-stat-tracker-app/src/app/components/track-game.tsx
+++ vollball-stat-tracker-app/src/app/components/track-game.tsx
@@ -1,90 +1,84 @@
-import { Button, Container, Grid, Typography } from '@mui/material';
-import { useReducer } from 'react';
-import { VbActionResult } from '../../models/VBActionResult';
+import { Container, Grid } from '@mui/material';
+import { useDispatch } from 'react-redux';
+import { statKeeperActions } from '../../redux/stats-keeper';
 import ActionButtonGroup from './action-button-group';
 
-type VolleyBallStats = {
-  attack: VbActionResult;
-  block: VbActionResult;
-  serve: VbActionResult;
-  reception: VbActionResult;
-  dig: VbActionResult;
-  set: VbActionResult;
-};
-
-const initialState: VolleyBallStats = {
-  attack: {
-    point: 0,
-    success: 0,
-    error: 0,
-  },
-  block: {
-    point: 0,
-    success: 0,
-    error: 0,
-  },
-  serve: {
-    point: 0,
-    success: 0,
-    error: 0,
-  },
-  reception: {
-    success: 0,
-    error: 0,
-  },
-  dig: {
-    success: 0,
-    error: 0,
-  },
-  set: {
-    point: 0,
-    success: 0,
-    error: 0,
-  },
-};
-
-type ACTIONTYPE =
-  | { type: 'success'; payload: number }
-  | { type: 'error'; payload: number };
-
-function reducer(state: typeof initialState, action: ACTIONTYPE) {
-  switch (action.type) {
-    case 'success':
-      return {
-        ...state,
-        attack: {
-          ...state.attack,
-          success: state.attack.success + action.payload,
-        },
-      };
-    case 'error':
-      state.attack.error += action.payload;
-      return state;
-    default:
-      throw new Error();
-  }
-}
-
 const TrackGame = () => {
-  const [state, dispatch] = useReducer(reducer, initialState);
-
-  const attackSuccess = () => {
-    dispatch({ type: 'success', payload: 1 });
-  };
+  const dispatch = useDispatch();
 
   return (
     <Container>
-      <Typography variant="h4">{state.attack.success}</Typography>
-      <Button variant="contained" onClick={attackSuccess}>
-        Attack Good
-      </Button>
       <Grid container spacing={2}>
-        <ActionButtonGroup title="Attack" pointEnabled />
-        <ActionButtonGroup title="Block" pointEnabled />
-        <ActionButtonGroup title="Serve" pointEnabled />
-        <ActionButtonGroup title="Set" pointEnabled />
-        <ActionButtonGroup title="Reception" />
-        <ActionButtonGroup title="Dig" />
+        <ActionButtonGroup
+          title="Attack"
+          pointEnabled
+          handlePointClick={() =>
+            dispatch(statKeeperActions.incrementAttackPoint())
+          }
+          handleSuccessClick={() => {
+            dispatch(statKeeperActions.incrementAttackSuccess());
+          }}
+          handleErrorClick={() => {
+            dispatch(statKeeperActions.incrementAttackError());
+          }}
+        />
+        <ActionButtonGroup
+          title="Block"
+          pointEnabled
+          handlePointClick={() =>
+            dispatch(statKeeperActions.incrementBlockPoint())
+          }
+          handleSuccessClick={() => {
+            dispatch(statKeeperActions.incrementBlockSuccess());
+          }}
+          handleErrorClick={() => {
+            dispatch(statKeeperActions.incrementBlockError());
+          }}
+        />
+        <ActionButtonGroup
+          title="Serve"
+          pointEnabled
+          handlePointClick={() =>
+            dispatch(statKeeperActions.incrementServePoint())
+          }
+          handleSuccessClick={() => {
+            dispatch(statKeeperActions.incrementServeSuccess());
+          }}
+          handleErrorClick={() => {
+            dispatch(statKeeperActions.incrementServeError());
+          }}
+        />
+        <ActionButtonGroup
+          title="Set"
+          pointEnabled
+          handlePointClick={() =>
+            dispatch(statKeeperActions.incrementSetPoint())
+          }
+          handleSuccessClick={() => {
+            dispatch(statKeeperActions.incrementSetSuccess());
+          }}
+          handleErrorClick={() => {
+            dispatch(statKeeperActions.incrementSetError());
+          }}
+        />
+        <ActionButtonGroup
+          title="Reception"
+          handleSuccessClick={() => {
+            dispatch(statKeeperActions.incrementReceptionSuccess());
+          }}
+          handleErrorClick={() => {
+            dispatch(statKeeperActions.incrementReceptionError());
+          }}
+        />
+        <ActionButtonGroup
+          title="Dig"
+          handleSuccessClick={() => {
+            dispatch(statKeeperActions.incrementDigSuccess());
+          }}
+          handleErrorClick={() => {
+            dispatch(statKeeperActions.incrementDigError());
+          }}
+        />
       </Grid>
     </Container>
   );
