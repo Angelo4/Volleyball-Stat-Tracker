01c39d07f950fe8bb9aee89211ea56fc0384cea4
Angelo Alcantara <angeloalcantara4@gmail.com>
2022-08-17 23:57:09 +1200

    Create options button group for action center

--- volleyball-stat-tracker-app/src/components/vb_action_center/ActionCenterContainer.tsx
+++ volleyball-stat-tracker-app/src/components/vb_action_center/ActionCenterContainer.tsx
@@ -1,97 +1,111 @@
-import { Button, Card, CardContent, Grid } from '@mui/material';
+import {
+  Button,
+  ButtonGroup,
+  Card,
+  CardContent,
+  Grid,
+  Typography,
+} from '@mui/material';
 import { useDispatch } from 'react-redux';
 import { statKeeperActions } from '../../redux/StatsKeeper';
 import ActionButtonGroup from './ActionButtonGroup';
 import { ActionCreators } from 'redux-undo';
+import { Undo, Redo, MoreHoriz } from '@mui/icons-material';
 
 const ActionCenterContainer = () => {
   const dispatch = useDispatch();
 
   return (
     <Grid item xs={12}>
-      <Card>
-        <CardContent>
-          <Grid container spacing={2}>
-            <ActionButtonGroup
-              title="Attack"
-              pointEnabled
-              handlePointClick={() =>
-                dispatch(statKeeperActions.incrementAttackPoint())
-              }
-              handleSuccessClick={() => {
-                dispatch(statKeeperActions.incrementAttackSuccess());
-              }}
-              handleErrorClick={() => {
-                dispatch(statKeeperActions.incrementAttackError());
-              }}
-            />
-            <ActionButtonGroup
-              title="Block"
-              pointEnabled
-              handlePointClick={() =>
-                dispatch(statKeeperActions.incrementBlockPoint())
-              }
-              handleSuccessClick={() => {
-                dispatch(statKeeperActions.incrementBlockSuccess());
-              }}
-              handleErrorClick={() => {
-                dispatch(statKeeperActions.incrementBlockError());
-              }}
-            />
-            <ActionButtonGroup
-              title="Serve"
-              pointEnabled
-              handlePointClick={() =>
-                dispatch(statKeeperActions.incrementServePoint())
-              }
-              handleSuccessClick={() => {
-                dispatch(statKeeperActions.incrementServeSuccess());
-              }}
-              handleErrorClick={() => {
-                dispatch(statKeeperActions.incrementServeError());
-              }}
-            />
-            <ActionButtonGroup
-              title="Set"
-              pointEnabled
-              handlePointClick={() =>
-                dispatch(statKeeperActions.incrementSetPoint())
-              }
-              handleSuccessClick={() => {
-                dispatch(statKeeperActions.incrementSetSuccess());
-              }}
-              handleErrorClick={() => {
-                dispatch(statKeeperActions.incrementSetError());
-              }}
-            />
-            <ActionButtonGroup
-              title="Reception"
-              handleSuccessClick={() => {
-                dispatch(statKeeperActions.incrementReceptionSuccess());
-              }}
-              handleErrorClick={() => {
-                dispatch(statKeeperActions.incrementReceptionError());
-              }}
-            />
-            <ActionButtonGroup
-              title="Dig"
-              handleSuccessClick={() => {
-                dispatch(statKeeperActions.incrementDigSuccess());
-              }}
-              handleErrorClick={() => {
-                dispatch(statKeeperActions.incrementDigError());
-              }}
-            />
+      <Grid container spacing={2}>
+        <ActionButtonGroup
+          title="Attack"
+          pointEnabled
+          handlePointClick={() =>
+            dispatch(statKeeperActions.incrementAttackPoint())
+          }
+          handleSuccessClick={() => {
+            dispatch(statKeeperActions.incrementAttackSuccess());
+          }}
+          handleErrorClick={() => {
+            dispatch(statKeeperActions.incrementAttackError());
+          }}
+        />
+        <ActionButtonGroup
+          title="Block"
+          pointEnabled
+          handlePointClick={() =>
+            dispatch(statKeeperActions.incrementBlockPoint())
+          }
+          handleSuccessClick={() => {
+            dispatch(statKeeperActions.incrementBlockSuccess());
+          }}
+          handleErrorClick={() => {
+            dispatch(statKeeperActions.incrementBlockError());
+          }}
+        />
+        <ActionButtonGroup
+          title="Serve"
+          pointEnabled
+          handlePointClick={() =>
+            dispatch(statKeeperActions.incrementServePoint())
+          }
+          handleSuccessClick={() => {
+            dispatch(statKeeperActions.incrementServeSuccess());
+          }}
+          handleErrorClick={() => {
+            dispatch(statKeeperActions.incrementServeError());
+          }}
+        />
+        <ActionButtonGroup
+          title="Set"
+          pointEnabled
+          handlePointClick={() =>
+            dispatch(statKeeperActions.incrementSetPoint())
+          }
+          handleSuccessClick={() => {
+            dispatch(statKeeperActions.incrementSetSuccess());
+          }}
+          handleErrorClick={() => {
+            dispatch(statKeeperActions.incrementSetError());
+          }}
+        />
+        <ActionButtonGroup
+          title="Reception"
+          handleSuccessClick={() => {
+            dispatch(statKeeperActions.incrementReceptionSuccess());
+          }}
+          handleErrorClick={() => {
+            dispatch(statKeeperActions.incrementReceptionError());
+          }}
+        />
+        <ActionButtonGroup
+          title="Dig"
+          handleSuccessClick={() => {
+            dispatch(statKeeperActions.incrementDigSuccess());
+          }}
+          handleErrorClick={() => {
+            dispatch(statKeeperActions.incrementDigError());
+          }}
+        />
+        <Grid item xs={12}>
+          <ButtonGroup>
             <Button
               onClick={() => {
                 dispatch(ActionCreators.undo());
               }}
             >
-              Undo
+              <Undo />
             </Button>
-          </Grid>
-        </CardContent>
-      </Card>
+            <Button>
+              <Redo />
+            </Button>
+            <Button>
+              <MoreHoriz />
+            </Button>
+          </ButtonGroup>
+        </Grid>
+      </Grid>
     </Grid>
   );
 };
