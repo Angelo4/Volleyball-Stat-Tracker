2f5ba903d63491c620b203c8d1bc7c58a12458d7
Angelo Alcantara <angeloalcantara4@gmail.com>
2022-08-18 12:01:10 +1200

    Add first implementation of snackbars. TODO: Refactor

--- volleyball-stat-tracker-app/src/components/vb_action_center/ActionCenterOptions.tsx
+++ volleyball-stat-tracker-app/src/components/vb_action_center/ActionCenterOptions.tsx
@@ -1,51 +1,119 @@
 import { MoreHoriz, Redo, Undo } from '@mui/icons-material';
-import { Button, ButtonGroup, Menu, MenuItem, Tooltip } from '@mui/material';
+import {
+  Button,
+  ButtonGroup,
+  Menu,
+  MenuItem,
+  Snackbar,
+  Tooltip,
+} from '@mui/material';
 import React from 'react';
+import MuiAlert, { AlertProps } from '@mui/material/Alert';
 import { useDispatch } from 'react-redux';
 import { ActionCreators } from 'redux-undo';
 import { statKeeperActions } from '../../redux/StatsKeeper';
 
+const Alert = React.forwardRef<HTMLDivElement, AlertProps>(function Alert(
+  props,
+  ref,
+) {
+  return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+});
+
 const ActionCentreOptions = () => {
   const dispatch = useDispatch();
 
+  // Snackbars
+  const [undoSnackbarOpen, setUndoSnackbarOpen] = React.useState(false);
+  const [redoSnackbarOpen, setRedoSnackbarOpen] = React.useState(false);
+
+  const handleUndoClick = () => {
+    dispatch(ActionCreators.undo());
+    setUndoSnackbarOpen(true);
+  };
+
+  const handleRedoClick = () => {
+    dispatch(ActionCreators.redo());
+    setRedoSnackbarOpen(true);
+  };
+
+  const handleUndoSnackbarClose = (
+    event?: React.SyntheticEvent | Event,
+    reason?: string,
+  ) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+    setUndoSnackbarOpen(false);
+  };
+
+  const handleRedoSnackbarClose = (
+    event?: React.SyntheticEvent | Event,
+    reason?: string,
+  ) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+    setRedoSnackbarOpen(false);
+  };
+
+  // Options Menu
   const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
-  const open = Boolean(anchorEl);
+  const menuOpen = Boolean(anchorEl);
   const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
     setAnchorEl(event.currentTarget);
   };
-  const handleClose = () => {
+  const handleMenuClose = () => {
     setAnchorEl(null);
   };
   const handleClearClick = () => {
     dispatch(statKeeperActions.clearState());
-    handleClose();
+    handleMenuClose();
   };
 
   return (
     <ButtonGroup>
       <Tooltip title="Undo">
-        <Button
-          onClick={() => {
-            dispatch(ActionCreators.undo());
-          }}
-        >
+        <Button onClick={handleUndoClick}>
           <Undo />
         </Button>
       </Tooltip>
-      <Tooltip title="Redo">
-        <Button
-          onClick={() => {
-            dispatch(ActionCreators.redo());
-          }}
+      <Snackbar
+        open={undoSnackbarOpen}
+        autoHideDuration={3000}
+        onClose={handleUndoSnackbarClose}
+      >
+        <Alert
+          onClose={handleUndoSnackbarClose}
+          severity="info"
+          sx={{ width: '100%' }}
         >
+          Undo successful
+        </Alert>
+      </Snackbar>
+      <Tooltip title="Redo">
+        <Button onClick={handleRedoClick}>
           <Redo />
         </Button>
       </Tooltip>
+      <Snackbar
+        open={redoSnackbarOpen}
+        autoHideDuration={3000}
+        onClose={handleRedoSnackbarClose}
+      >
+        <Alert
+          onClose={handleRedoSnackbarClose}
+          severity="info"
+          sx={{ width: '100%' }}
+        >
+          Redo successful
+        </Alert>
+      </Snackbar>
       <Button
         id="basic-button"
-        aria-controls={open ? 'basic-menu' : undefined}
+        aria-controls={menuOpen ? 'basic-menu' : undefined}
         aria-haspopup="true"
-        aria-expanded={open ? 'true' : undefined}
+        aria-expanded={menuOpen ? 'true' : undefined}
         onClick={handleClick}
       >
         <MoreHoriz />
@@ -53,8 +121,8 @@ const ActionCentreOptions = () => {
       <Menu
         id="basic-menu"
         anchorEl={anchorEl}
-        open={open}
-        onClose={handleClose}
+        open={menuOpen}
+        onClose={handleMenuClose}
         MenuListProps={{
           'aria-labelledby': 'basic-button',
         }}
