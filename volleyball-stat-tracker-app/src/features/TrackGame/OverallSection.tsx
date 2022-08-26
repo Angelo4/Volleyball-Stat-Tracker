baee0f687cceb38a9aaabc8aa80fbd924b178354
Angelo Alcantara <angeloalcantara4@gmail.com>
2022-08-26 23:02:16 +1200

    Play around with polar graph

--- volleyball-stat-tracker-app/src/features/TrackGame/OverallSection.tsx
+++ volleyball-stat-tracker-app/src/features/TrackGame/OverallSection.tsx
@@ -1,4 +1,5 @@
 import { Grid } from '@mui/material';
+import OverallRadarChart from '../../components/vb_stat_display/OverallRadarChart';
 import OverallTable from '../../components/vb_stat_display/OverallTable';
 import StatsStackGraph from '../../components/vb_stat_display/StatsStackGraph';
 
@@ -11,6 +12,9 @@ export const OverallSection = () => {
       <Grid item xs={12} md={12} lg={6}>
         <StatsStackGraph />
       </Grid>
+      {/* <Grid item xs={12}>
+        <OverallRadarChart />
+      </Grid> */}
     </Grid>
   );
 };
