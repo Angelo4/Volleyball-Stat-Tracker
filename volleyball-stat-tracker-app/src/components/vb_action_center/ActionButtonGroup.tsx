2ad1c675b232622066eba72513da2ac21dd3f569
Angelo Alcantara <angeloalcantara4@gmail.com>
2022-09-03 17:30:39 +1200

    Add snackbar to action center button group

--- volleyball-stat-tracker-app/src/components/vb_action_center/ActionButtonGroup.tsx
+++ volleyball-stat-tracker-app/src/components/vb_action_center/ActionButtonGroup.tsx
@@ -1,4 +1,6 @@
 import { Button, ButtonGroup, Grid, Typography } from '@mui/material';
+import { useState } from 'react';
+import ActionSnackbar from './ActionSnackbar';
 
 type ActionButtonGroupProps = {
   title: string;
@@ -9,37 +11,79 @@ type ActionButtonGroupProps = {
 };
 
 const ActionButtonGroup = (props: ActionButtonGroupProps) => {
+  const [errorSnackbarOpen, setErrorSnackbarOpen] = useState<boolean>(false);
+  const [successSnackbarOpen, setSuccessSnackbarOpen] =
+    useState<boolean>(false);
+  const [pointSnackbarOpen, setPointSnackbarOpen] = useState<boolean>(false);
+
+  const onErrorClick = () => {
+    props.handleErrorClick();
+    setErrorSnackbarOpen(true);
+  };
+
+  const onSuccessClick = () => {
+    props.handleSuccessClick();
+    setSuccessSnackbarOpen(true);
+  };
+
+  const onPointClick = () => {
+    if (props.handlePointClick) {
+      props.handlePointClick();
+      setPointSnackbarOpen(true);
+    }
+  };
+
   return (
-    <Grid
-      container
-      spacing={2}
-      sx={{
-        alignItems: 'center',
-        justifyContent: 'center',
-        display: 'flex',
-      }}
-    >
-      <Grid item xs={12}>
-        <Typography variant="h5" align="center">
-          {props.title}
-        </Typography>
-      </Grid>
-      <Grid item xs={12}>
-        <ButtonGroup variant="outlined" fullWidth>
-          <Button color="error" onClick={props.handleErrorClick} fullWidth>
-            Error
-          </Button>
-          <Button onClick={props.handleSuccessClick} fullWidth>
-            Success
-          </Button>
-          {!!props.pointEnabled && (
-            <Button color="success" onClick={props.handlePointClick} fullWidth>
-              Point
+    <>
+      <Grid
+        container
+        spacing={2}
+        sx={{
+          alignItems: 'center',
+          justifyContent: 'center',
+          display: 'flex',
+        }}
+      >
+        <Grid item xs={12}>
+          <Typography variant="h5" align="center">
+            {props.title}
+          </Typography>
+        </Grid>
+        <Grid item xs={12}>
+          <ButtonGroup variant="outlined" fullWidth>
+            <Button color="error" onClick={onErrorClick} fullWidth>
+              Error
             </Button>
-          )}
-        </ButtonGroup>
+            <Button onClick={onSuccessClick} fullWidth>
+              Success
+            </Button>
+            {!!props.pointEnabled && (
+              <Button color="success" onClick={onPointClick} fullWidth>
+                Point
+              </Button>
+            )}
+          </ButtonGroup>
+        </Grid>
       </Grid>
-    </Grid>
+      <ActionSnackbar
+        message="Error added"
+        severity="error"
+        open={errorSnackbarOpen}
+        setOpen={setErrorSnackbarOpen}
+      />
+      <ActionSnackbar
+        message="Attempt successful"
+        severity="info"
+        open={successSnackbarOpen}
+        setOpen={setSuccessSnackbarOpen}
+      />
+      <ActionSnackbar
+        message="Point scored!"
+        severity="success"
+        open={pointSnackbarOpen}
+        setOpen={setPointSnackbarOpen}
+      />
+    </>
   );
 };
 
