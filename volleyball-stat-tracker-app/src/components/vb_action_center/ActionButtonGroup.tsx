72ae9b63fc71e23b8ca20faa9d505e44cab9c64a
Angelo Alcantara <angeloalcantara4@gmail.com>
2022-09-11 17:28:01 +1200

    revert changes from tests

--- volleyball-stat-tracker-app/src/components/vb_action_center/ActionButtonGroup.tsx
+++ volleyball-stat-tracker-app/src/components/vb_action_center/ActionButtonGroup.tsx
@@ -55,7 +55,7 @@ const ActionButtonGroup = (props: ActionButtonGroupProps) => {
               Error
             </Button>
             <Button onClick={onSuccessClick} fullWidth>
-              Successs
+              Success
             </Button>
             {!!props.pointEnabled && (
               <Button color="success" onClick={onPointClick} fullWidth>
