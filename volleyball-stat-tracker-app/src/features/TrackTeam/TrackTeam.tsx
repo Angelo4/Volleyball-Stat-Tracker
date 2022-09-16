b54a1439c15c4598defb9c9d24bb7ab58d25bcbb
Angelo Alcantara <angeloalcantara4@gmail.com>
2022-09-16 22:37:44 +1200

    Testing out if diffs show between envs

--- volleyball-stat-tracker-app/src/features/TrackTeam/TrackTeam.tsx
+++ volleyball-stat-tracker-app/src/features/TrackTeam/TrackTeam.tsx
@@ -3,7 +3,11 @@ import { Typography } from '@mui/material';
 type Props = {};
 
 const TrackTeam = (props: Props) => {
-  return <Typography variant='h5'>Team tracking coming soon...</Typography>;
+  return (
+    <Typography variant='h5'>
+      Team tracking coming soon... This should only be in test env
+    </Typography>
+  );
 };
 
 export default TrackTeam;
